46838697bef6d56a9ee070496ceedcae03e184eb
PC-ORBIT <PC-ORBIT@DESKTOP-IK6P0R2>
2026-02-19 07:53:51 +0200

    init

---
+++ lib/core/app_colors.dart
@@ -0,0 +1,9 @@
+import 'dart:ui';
+
+class AppColors {
+  static const Color darkGrey = Color(0xFF29384D);
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color lightBlue = Color(0xFFE2F4F6);
+  static const Color gold = Color(0xFFFFF1D4);
+
+}
